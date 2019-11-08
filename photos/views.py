fca4ee5c2aca2ead36e6c9e0a94af36086088136
Ngina07 <phillis.njoroge@strathmore.edu>
2019-11-08 15:37:47 +0300

    add dynamic links and test if they are working

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
+
+def welcome(request):
+    return render(request, 'images.html')
 
 # Create your views here.
