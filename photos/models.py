67783dcd71aa1423fdf238d9c851966a63942ea5
Ngina07 <phillis.njoroge@strathmore.edu>
2019-11-09 13:09:30 +0300

    create migration file and migrate model changes to database

--- photos/models.py
+++ photos/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Location(models.Model):
-    location = models.charField(max_length=100)
+    location = models.CharField(max_length=100)
 
     def __str__(self):
         return self.location
