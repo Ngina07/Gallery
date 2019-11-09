fa2f2a9753108363d8694919d8f5ab1ba6999213
Ngina07 <phillis.njoroge@strathmore.edu>
2019-11-09 16:43:15 +0300

    add method to display all images uploaded

--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
+from .models import Image
+
 
 def welcome(request):
     return render(request, 'images.html')
 
 # Create your views here.
+
+def images(request):
+    '''
+    Method to display all images uploade
+    '''
+    images = Image.objects.all()
+
+    return render (request, 'images.html',{"images":images})
