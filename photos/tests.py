3ae75385a65d4f7f07f759e3779e70a2a8e38278
Ngina07 <phillis.njoroge@strathmore.edu>
2019-11-09 17:15:58 +0300

    add tests for the categories instances and methods

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import Location,categories,Image
 
-# Create your tests here.
+
+class categoriesTestClass(TestCase):
+    def setUp(self):
+        self.Random = categories(category = 'Random')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Random,categories))
+
+    def tearDown(self):
+        categories.objects.all().delete()
+
+    def test_save_method(self):
+        self.Random.save_category()
+        category = categories.objects.all()
+        self.assertTrue(len(category)>0)
+    def test_delete_method(self):
+        self.Random.delete_category('Random')
+        category = categories.objects.all()
+        self.assertTrue(len(category)==0)
