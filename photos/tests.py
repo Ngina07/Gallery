a3cf3d08ca13034ea476466402912a1b09cbf2ac
Ngina07 <phillis.njoroge@strathmore.edu>
2019-11-09 17:30:29 +0300

    add tests for the Location class  instances and methods

--- photos/tests.py
+++ photos/tests.py
@@ -20,3 +20,23 @@ class categoriesTestClass(TestCase):
         self.Random.delete_category('Random')
         category = categories.objects.all()
         self.assertTrue(len(category)==0)
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.Somewhere = Location(location = 'Somewhere')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Somewhere,Location))
+
+    def tearDown(self):
+        Location.objects.all().delete()
+
+    def test_save_method(self):
+        self.Somewhere.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)>0)
+
+    def test_delete_method(self):
+        self.Somewhere.delete_location('Somewhere')
+        location = Location.objects.all()
+        self.assertTrue(len(location)==0)
